76aa3150da8298ad7fba912d8a2a8981c6a08c86
jangorecki <j.gorecki@wit.edu.pl>
2016-01-09 14:10:23 +0000

    point-in-time and better readme.md

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-# pg
+# pg [![Build Status](https://travis-ci.org/jangorecki/pg.svg?branch=master)](https://travis-ci.org/jangorecki/pg)
 
 Postgres utilities for R language
 
 - based on [RPostgreSQL](https://github.com/tomoakin/RPostgreSQL)
 - logging powered by [logR](https://github.com/jangorecki/logR)
 
-Features:  
+## Features
 
 - wrappers on many commonly used `DBI::db*` functions, enables logging, default `conn` and other arguments
 - new functions: `pgExistsSchema`, `pgTruncateTable`, `pgListTableColumns`, `pgDropSchema`, `pgListTableColumns` and potentially more in future
@@ -13,10 +13,79 @@ Features:
 - metadata for logging with logR can store custom arbitrary information
 - helpers for processing batches of the data
 
-How to use:  
+## Installation
 
-- install as R package: `install.packages("pg", repos = "soon")`, `devtools::install_github("jangorecki/pg")`
-- follow workflow examples in [tests/tests.R](tests/tests.R)
-- full environment can be easily reproduced from CI yaml file
+R
+```r
+library(devtools)
+install_github("jangorecki/pg")
+```
+
+postgres
+```r
+docker run -it --rm -p 5432 -e POSTGRES_USER=r_user -e POSTGRES_PASSWORD=r_password -e POSTGRES_DB=r_db -e POSTGRES_PORT=5432 --name pg postgres:9.5
+```
+
+## Setup
+
+```r
+# Sys.setenv(POSTGRES_HOST="172.17.0.2") # use this for non-localhost postgres
+
+suppressPackageStartupMessages({
+    library(data.table)
+    library(logR)
+    library(pg)
+})
+
+options("pg.conn" = pgConnect(dbname = "r_db", user = "r_user", password = "r_password"))
+
+# logR setup
+meta = function(run_id=get_run_id(), r_user = as.character(Sys.info()[["user"]])[1L], r_timestamp = Sys.time(), r_fun = NA_character_, r_args = NA_character_) list(run_id=run_id, r_user=r_user, r_timestamp=r_timestamp, r_fun=r_fun, r_args=paste(r_args, collapse=","))
+create_meta = list(run_id = "INTEGER", r_user = "VARCHAR(255)", r_timestamp = "TIMESTAMPTZ", r_fun = "VARCHAR(255)", r_args = "VARCHAR(255)")
+options("logR.conn" = getOption("pg.conn"),
+        "logR.schema" = "r_tech",
+        "logR.table" = "logr",
+        "logR.meta" = meta)
+
+# restart schemas
+pgDropSchema(c("r_tech","r_data"), cascade = TRUE, silent = TRUE, .log = FALSE)
+options("run_id" = NULL)
+options("digits.secs" = 3L)
+logR_schema(meta = create_meta)
+create_run_table(schema_name = "r_tech", table_name = "run", .log = FALSE)
+```
+
+## Use
+
+```r
+pgSendQuery("CREATE SCHEMA r_data;")
+
+# loading data in batches
+options("run_id" = NULL)
+dt = data.table(a = 1:2, b = letters[2:1])
+pgWriteTable(c("r_data","techstamp"), dt)
+
+options("run_id" = new_run_id(.log = FALSE))
+dt = data.table(a = 4:6, b = letters[6:4])
+pgWriteTable(c("r_data","techstamp"), dt)
+
+options("run_id" = new_run_id(.log = FALSE))
+dt = data.table(a = 5:8, b = letters[8:5])
+pgWriteTable(c("r_data","techstamp"), dt)
+
+# data
+pgReadTable(c("r_data","techstamp"))
+
+# logs
+pgReadTable(c("r_tech","logr"), .log = FALSE)
+pgReadTable(c("r_tech","logr"), .log = FALSE)[, .(in_rows, out_rows), .(run_id, r_fun, r_args)]
+
+# temporal 'latest' query using tech timestamp
+pgGetQuery("SELECT DISTINCT ON (a) * FROM r_data.techstamp ORDER BY a, r_timestamp DESC;")
+```
+
+For more examples and full reproducible environment see CI workflow and [tests/tests.R](tests/tests.R). Be aware travis-ci uses 5433 port instead of default 5432, this can easily be controlled by environment variables as demonstrated in travis yaml.  
+
+## Notes
 
 Only postgres 9.5+ support is planned, currently there is no 9.5+ specific code yet.  
--- tests/tests.R
+++ tests/tests.R
@@ -1,5 +1,5 @@
 ## docker users should be able to setup local environment using following
-# docker run -it --rm -p 5432 -e POSTGRES_USER=r_user -e POSTGRES_PASSWORD=r_password -e POSTGRES_DB=r_db --name pg postgres:9.5
+# docker run -it --rm -p 5432 -e POSTGRES_USER=r_user -e POSTGRES_PASSWORD=r_password -e POSTGRES_DB=r_db -e POSTGRES_PORT=5432 --name pg postgres:9.5
 # Sys.setenv(POSTGRES_HOST="172.17.0.2")
 
 suppressPackageStartupMessages({
@@ -24,7 +24,10 @@ options("digits.secs" = 3L)
 logR_schema(meta = create_meta)
 
 # expected tables and column count in db
-stopifnot(all.equal(pgListTableColumns(.log=FALSE)[, .N, .(table_schema, table_name)], data.table(table_schema="r_tech", table_name="logr", N=18L, key = c("table_schema","table_name"))))
+stopifnot(all.equal(
+    pgListTableColumns(.log=FALSE)[, .N, .(table_schema, table_name)],
+    data.table(table_schema="r_tech", table_name="logr", N=18L, key = c("table_schema","table_name"))
+))
 
 # expected row count and status content with and without `.log` argument, valid only for sequence run after rebuilding schema
 stopifnot(identical(pgGetQuery("SELECT * FROM r_tech.logr;")$status, NA_character_))
@@ -32,19 +35,28 @@ stopifnot(identical(pgGetQuery("SELECT * FROM r_tech.logr;", .log = FALSE)$statu
 
 # create `run` table, validate created count of column
 create_run_table(schema_name = "r_tech", table_name = "run", .log = FALSE)
-stopifnot(all.equal(pgListTableColumns(.log=FALSE)[, .N, .(table_schema, table_name)], data.table(table_schema=rep("r_tech",2L), table_name=c("logr","run"), N=c(18L,3L), key = c("table_schema","table_name"))))
+stopifnot(all.equal(
+    pgListTableColumns(.log=FALSE)[, .N, .(table_schema, table_name)],
+    data.table(table_schema=rep("r_tech",2L), table_name=c("logr","run"), N=c(18L,3L), key = c("table_schema","table_name"))
+))
 
 # create schema data
 pgSendQuery("CREATE SCHEMA r_data;")
 
 # validate 2 currently written logs
-stopifnot(all.equal(pgGetQuery("SELECT * FROM r_tech.logr;", .log = FALSE)[, .N, .(r_fun, status)], data.table(r_fun=c("dbGetQuery","dbSendQuery"), status=rep("success",2L), N=c(1L,1L))))
+stopifnot(all.equal(
+    pgGetQuery("SELECT * FROM r_tech.logr;", .log = FALSE)[, .N, .(r_fun, status)],
+    data.table(r_fun=c("dbGetQuery","dbSendQuery"), status=rep("success",2L), N=c(1L,1L))
+))
 
 # validate techstamp and nrows
 dt = data.table(a = 1:5, b = letters[5:1])
 pgWriteTable(c("r_data","non_techstamp"), dt, techstamp = FALSE)
 pgWriteTable(c("r_data","techstamp"), dt, techstamp = TRUE)
-stopifnot(identical(dim(pgReadTable(c("r_data","techstamp"))), 5:4), identical(dim(pgReadTable(c("r_data","non_techstamp"))), c(5L,2L)))
+stopifnot(
+    identical(dim(pgReadTable(c("r_data","techstamp"))), 5:4),
+    identical(dim(pgReadTable(c("r_data","non_techstamp"))), c(5L,2L))
+)
 
 # validate truncate
 pgTruncateTable(c("r_data","non_techstamp"))
@@ -52,20 +64,26 @@ stopifnot(pgGetQuery("SELECT count(*) cnt FROM r_data.non_techstamp;")$cnt==0L)
 
 # validate run_id batches
 pgTruncateTable(c("r_data","techstamp"))
-dt = data.table(a = 1:2, b = letters[2:1])
+dt = data.table(a = 1:4, b = letters[4:1])
 pgWriteTable(c("r_data","techstamp"), dt) # options("run_id" = NULL) # should be already with this value while running test
 options("run_id" = new_run_id(.log = FALSE))
-dt = data.table(a = 4:6, b = letters[6:4])
+dt = data.table(a = 3:7, b = letters[7:3])
 pgWriteTable(c("r_data","techstamp"), dt)
 options("run_id" = new_run_id(.log = FALSE))
-dt = data.table(a = 5:8, b = letters[8:5])
+dt = data.table(a = 8:10, b = letters[10:8])
 pgWriteTable(c("r_data","techstamp"), dt)
 
 # validate number of rows per batch
-stopifnot(all.equal(pgReadTable(c("r_data","techstamp"), .log = FALSE)[, .N, run_id], data.table(run_id = c(NA, 1L, 2L), N = 2:4)))
+stopifnot(all.equal(
+    pgReadTable(c("r_data","techstamp"), .log = FALSE)[, .N, run_id],
+    data.table(run_id = c(NA, 1L, 2L), N = c(4L,5L,3L))
+))
 
 # validate logs on writing batches
-stopifnot(all.equal(pgReadTable(c("r_tech","logr"), .log = FALSE)[(length(logr_id)-2L):length(logr_id), .(in_rows, .N), run_id], data.table(run_id = c(NA, 1L, 2L), in_rows = 2:4, N = rep(1L,3L))))
+stopifnot(all.equal(
+    pgReadTable(c("r_tech","logr"), .log = FALSE)[(length(logr_id)-2L):length(logr_id), .(in_rows, .N), run_id],
+    data.table(run_id = c(NA, 1L, 2L), in_rows = c(4L,5L,3L), N = rep(1L,3L))
+))
 
 # temporal query: latest
 stopifnot(all.equal(
@@ -74,9 +92,16 @@ stopifnot(all.equal(
 ))
 
 # temporal query: point-in-time
-
+ts = pgGetQuery("SELECT DISTINCT r_timestamp FROM r_data.techstamp WHERE run_id = 1;", .log = FALSE)$r_timestamp + 0.005
+stopifnot(all.equal(
+    pgGetQuery(sprintf("SELECT DISTINCT ON (a) * FROM r_data.techstamp WHERE r_timestamp <= '%s'::TIMESTAMPTZ ORDER BY a, r_timestamp DESC;", ts)),
+    pgGetQuery("SELECT DISTINCT ON (a) * FROM r_data.techstamp WHERE run_id <= 1 OR run_id IS NULL ORDER BY a, r_timestamp DESC;")
+))
 
 # temporal query: difference
+
+
+# upsert
 
 
 q("no")
